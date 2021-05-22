2b40932f49d501e27b9ffd888ff03fdd98c88cf3
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-23 01:35:18 +0200

    parse incoming requests

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/add", (req, res, next) => {
-  res.send("<h1>Add Cool beans</h1>");
+  res.send(
+    "<form action='product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>"
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
