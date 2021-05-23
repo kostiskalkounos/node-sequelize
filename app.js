63582a4d1631ca4972a75383d680b498a77c6f95
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-23 15:33:52 +0200

    serve files statically

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const shopRoutes = require("./routes/shop");
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
