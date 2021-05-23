8ec0914634c0d63689b7766ed37b78e63c167f75
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-23 19:24:05 +0200

    share data across requests and users

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const express = require("express");
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
