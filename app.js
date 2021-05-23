33dae24ba801a3d68bbe6f0969c528fe2bd0238d
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-23 03:41:49 +0200

    add a 404 error page

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ const shopRoutes = require("./routes/shop");
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
+
 app.use(adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found!</h1>");
+});
 
 app.listen(3000);
