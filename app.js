eb21ab2e0760496bc0f16df6f5f3f0d35aed47bd
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-23 23:04:33 +0200

    implement Pug and output dynamic content

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const shopRoutes = require("./routes/shop");
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", "views"); // not needed, the default path is already '/views'
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -12,7 +15,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404");
 });
 
 app.listen(3000);
