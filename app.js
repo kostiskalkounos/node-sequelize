f083496c36e79ef4883bcfbaa1823ca5182c7bc7
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-24 04:23:55 +0200

    replace Handlebars with Ejs

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
 const path = require("path");
 const express = require("express");
-const expressHbs = require("express-handlebars");
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
 
-app.engine(
-  "hbs",
-  expressHbs({
-    layoutsDir: "views/layouts", // not needed, the default path is already that one
-    defaultLayout: "main-layout",
-    extname: "hbs",
-  })
-);
-
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.set("views", "views"); // not needed, the default path is already '/views'
 
 app.use(express.urlencoded({ extended: false }));
