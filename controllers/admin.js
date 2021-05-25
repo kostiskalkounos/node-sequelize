a8a5047ee90a51fc15f859492bfadcd668c1cbca
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-25 18:31:58 +0200

    fetch admin products

--- controllers/admin.js
+++ controllers/admin.js
@@ -70,10 +70,10 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows]) => {
+  Product.findAll()
+    .then((products) => {
       res.render("admin/products", {
-        prods: rows,
+        prods: products,
         pageTitle: "Admin Products",
         path: "/admin/products",
       });
