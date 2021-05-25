0b0a0864373c579d216c34954c37021ff775041a
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-25 05:09:03 +0200

    delete cart items

--- models/product.js
+++ models/product.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const Cart = require("./cart");
 
 const p = path.join(
   path.dirname(require.main.filename),
@@ -49,9 +50,10 @@ module.exports = class Product {
 
   static deleteById(id) {
     getProductsFromFile((products) => {
+      const product = products.find((prod) => prod.id === id);
       const updatedProducts = products.filter((prod) => prod.id !== id);
       fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-        console.log(err);
+        Cart.deleteProduct(id, product.price);
       });
       callback(product);
     });
