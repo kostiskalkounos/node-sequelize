539fa851263015240dfff7bb0d938f723afb7a5c
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-25 22:11:13 +0200

    add existing products and retrieving cart items

--- controllers/shop.js
+++ controllers/shop.js
@@ -84,6 +84,7 @@ exports.getCart = (req, res, next) => {
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   let fetchedCart;
+  let newQuantity = 1;
   req.user
     .getCart()
     .then((cart) => {
@@ -95,20 +96,17 @@ exports.postCart = (req, res, next) => {
       if (products.length > 0) {
         product = products[0];
       }
-      let newQuantity = 1;
       if (product) {
-        // ...
+        const oldQuantity = product.cartItem.quantity; // sequelize gives us access
+        newQuantity = oldQuantity + 1;
+        return product; // automatically wrapped by promise
       }
-      // Add a new product for the first time
-      return Product.findByPk(prodId)
-        .then((product) => {
-          return fetchedCart.addProduct(product, {
-            through: { quantity: newQuantity },
-          });
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+      return Product.findByPk(prodId);
+    })
+    .then((product) => {
+      return fetchedCart.addProduct(product, {
+        through: { quantity: newQuantity },
+      });
     })
     .then(() => {
       res.redirect("/cart");
