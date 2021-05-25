3d460470a241811f939c82a5b07c93d5901ea342
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-25 17:08:57 +0200

    define a Sequelize model

--- models/product.js
+++ models/product.js
@@ -1,33 +1,29 @@
-const Cart = require("./cart");
-const db = require("../util/database");
+const Sequelize = require("sequelize");
+const sequelize = require("../util/database");
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, price, description) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
+const Product = sequelize.define("product", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
 
-  save() {
-    // return a promise and use it where it's called
-    return db.execute(
-      // Insert data this way to guard against SQL injection in the input fields
-      "INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)",
-      [this.title, this.price, this.imageUrl, this.description]
-    );
-  }
-
-  static deleteById(id) {}
-
-  static fetchAll() {
-    // return a promise and use it where it's called
-    return db.execute("SELECT * FROM products");
-  }
-
-  static findById(id) {
-    // let SQL inject the value
-    return db.execute("SELECT * FROM products WHERE products.id = ?", [id]);
-  }
-};
+module.exports = Product;
