916b47c19f1f1f3f05de7553f81d8086ca22ea23
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-25 18:26:56 +0200

    get a single product in two ways

--- controllers/shop.js
+++ controllers/shop.js
@@ -18,17 +18,31 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(([product]) => {
+
+  Product.findByPk(prodId)
+    .then((product) => {
       res.render("shop/product-detail", {
-        product: product[0],
+        product: product,
         pageTitle: product.title,
         path: "/products",
       });
     })
     .catch((err) => {
-      console.log("err");
+      console.log(err);
     });
+
+  // Alternative way
+  // Product.findAll({ where: { id: prodId } })
+  //   .then((products) => {
+  //     res.render("shop/product-detail", {
+  //       product: products[0],
+  //       pageTitle: products[0].title,
+  //       path: "/products",
+  //     });
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
 };
 
 exports.getIndex = (req, res, next) => {
